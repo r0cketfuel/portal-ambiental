ba00468cc199544c3e79c3263538608bd7045e18
r0cketfuel <unleashedwings@gmail.com>
2024-03-18 12:36:58 -0300

    Fix botón BacktoTop

--- src/app/components/backToTop/backtotop.js
+++ src/app/components/backToTop/backtotop.js
@@ -1,13 +1,42 @@
+"use client"
 import "./module.css";
+import { useState, useEffect } from "react";
 
 export default function BackToTop() {
+	const [isVisible, setIsVisible] = useState(false);
+
+	useEffect(() => {
+		function handleScroll() {
+			if (window.pageYOffset > 300) {
+				setIsVisible(true);
+			} else {
+				setIsVisible(false);
+			}
+		}
+
+		window.addEventListener("scroll", handleScroll);
+
+		// Remueve el listener del evento de scroll al desmontar el componente
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
+
+	const scrollToTop = () => {
+		window.scrollTo({
+			top: 0,
+			behavior: "smooth"
+		});
+	};
+
 	return (
-        <div id="backToTop" className="backToTop">
-            <a>
-                <div className="backToTop-icon">
-                    <i className="fa-solid fa-chevron-up"></i>
-                </div>
-            </a>
-        </div>
+		<div
+			className={`backToTop ${isVisible ? "show" : ""}`}
+			onClick={scrollToTop}
+		>
+			<div className="backToTop-icon">
+				<i className="fa-solid fa-chevron-up"></i>
+			</div>
+		</div>
 	);
 }
