15d7e5dbb02a1fa0a60de16c0a801c81f95d225a
r0cketfuel <unleashedwings@gmail.com>
2024-03-20 11:42:16 -0300

    Rutas endpoints

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+
+    env: {
+        URL_API_BOYA_PBB0:process.env.URL_API_BOYA_PBB0,
+        URL_API_BOYA_PBB1:process.env.URL_API_BOYA_PBB1,
+        URL_API_BOYA_PBB2:process.env.URL_API_BOYA_PBB2
+    }
+};
 
 export default nextConfig;
